359bad6250aabc169009f0c786a4961eb2efc592
Lalit2210-create <lalitkumar72296@gmail.com>
2025-11-01 17:49:14 +0530

    part B Database table

---
+++ part B Database Table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Employee (
+    EmpID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Salary DOUBLE
+);
+
+INSERT INTO Employee VALUES (101, 'John Doe', 45000);
+INSERT INTO Employee VALUES (102, 'Riya Sharma', 55000);
+INSERT INTO Employee VALUES (103, 'Amit Kumar', 60000);
